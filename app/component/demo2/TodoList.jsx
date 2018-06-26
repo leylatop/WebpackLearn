5949332b5c4cd66a396df732cb838f0520c09ff3
乔晓欣 <xiaoxin.qiao@inin88.com>
2018-06-26 18:24:03 +0800

    Fix: 引入MockJs,实现假接口开发;ES7的Async/Await的使用

--- app/component/demo2/TodoList.jsx
+++ app/component/demo2/TodoList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import List from './List';
+// import apiRequest from '../../public/js/apiRequest';
+import "babel-polyfill";
+import apiRequestAsync from '../../public/js/apiRequestAsync';
 import '../../public/css/todoList.pcss';
 
 class TodoList extends React.Component {
@@ -29,8 +32,19 @@ class TodoList extends React.Component {
         this.setState({list: list})
     }
 
-    componentDidMount() {
+    // es7 async await
+    async handleTodoList() {
+        let todoList = await apiRequestAsync.post('todoList');
+        this.setState({list: todoList.list});
+        let todoList1 = await apiRequestAsync.post('todoList');
+        console.log(todoList1);
+        let todoList2 = await apiRequestAsync.post('todoList');
+        console.log(todoList2);
+    }
 
+    componentDidMount() {
+        this.handleTodoList();
+        // apiRequest.get('todoList', {}, data => this.setState({list: data.list}))
     }
 
     render() {
